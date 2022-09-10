220fb2dce0e9d529c9f767562fdc2659f5f734f8
Jon-Fredrik Hopland <jho107@post.uit.no>
2022-09-10 15:39:03 -0400

    Fixes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -21,11 +21,14 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _totTip = double.parse(_billController.text) * tipPercentage;
       _tot = double.parse(_billController.text) + _totTip;
-      _tipPerPerson = double.parse(_numPeopleController.text) != 0 ? _totTip / double.parse(_numPeopleController.text) : _totTip;
-      _totPerPerson = double.parse(_billController.text) / double.parse(_numPeopleController.text) + _tipPerPerson;
+      _tipPerPerson = double.parse(_numPeopleController.text) != 0
+          ? _totTip / double.parse(_numPeopleController.text)
+          : _totTip;
+      _totPerPerson = double.parse(_numPeopleController.text) != 0 ? double.parse(_billController.text) /
+              double.parse(_numPeopleController.text) +
+          _tipPerPerson : _tot;
     });
   }
-
 
   @override
   void initState() {
@@ -49,14 +52,17 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Card(
-          color: Colors.blue.shade50,
+          color: Colors.white,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: const InputDecoration(hintText: 'Bill amount'),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Bill amount',
+                    icon: Icon(Icons.attach_money),
+                  ),
                   controller: _billController,
                 ),
               ),
@@ -64,7 +70,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: const EdgeInsets.all(8.0),
                 child: TextField(
                   decoration: const InputDecoration(
-                      hintText: 'Number of people to split the bill'),
+                    hintText: 'Number of people to split the bill',
+                    icon: Icon(Icons.people),
+                  ),
                   controller: _numPeopleController,
                 ),
               ),
@@ -75,15 +83,21 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        ElevatedButton(onPressed: () {
-                          _calculate(0.10);
-                        }, child: const Text('10%')),
-                        ElevatedButton(onPressed: () {
-                          _calculate(0.15);
-                        }, child: const Text('15%')),
-                        ElevatedButton(onPressed: () {
-                          _calculate(0.18);
-                        }, child: const Text('18%')),
+                        ElevatedButton(
+                            onPressed: () {
+                              _calculate(0.10);
+                            },
+                            child: const Text('10%')),
+                        ElevatedButton(
+                            onPressed: () {
+                              _calculate(0.15);
+                            },
+                            child: const Text('15%')),
+                        ElevatedButton(
+                            onPressed: () {
+                              _calculate(0.18);
+                            },
+                            child: const Text('18%')),
                       ],
                     ),
                   ),
@@ -92,15 +106,21 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        ElevatedButton(onPressed: () {
-                          _calculate(0.20);
-                        }, child: const Text('20%')),
-                        ElevatedButton(onPressed: () {
-                          _calculate(0.22);
-                        }, child: const Text('22%')),
-                        ElevatedButton(onPressed: () {
-                          _calculate(0.25);
-                        }, child: const Text('25%'))
+                        ElevatedButton(
+                            onPressed: () {
+                              _calculate(0.20);
+                            },
+                            child: const Text('20%')),
+                        ElevatedButton(
+                            onPressed: () {
+                              _calculate(0.22);
+                            },
+                            child: const Text('22%')),
+                        ElevatedButton(
+                            onPressed: () {
+                              _calculate(0.25);
+                            },
+                            child: const Text('25%'))
                       ],
                     ),
                   ),
@@ -119,19 +139,73 @@ class _MyHomePageState extends State<MyHomePage> {
                         children: [
                           Padding(
                             padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text('Total tip: ${_totTip.toStringAsFixed(2)}'),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Total tip',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  _totTip.toStringAsFixed(2),
+                                  style: TextStyle(
+                                      color: Colors.blue.shade50,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                           Padding(
                             padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text('Total: ${_tot.toStringAsFixed(2)}'),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Total',
+                                    style: TextStyle(color: Colors.white)),
+                                Text(
+                                  _tot.toStringAsFixed(2),
+                                  style: TextStyle(
+                                      color: Colors.blue.shade50,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                           Padding(
                             padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text('Tip per person: ${_tipPerPerson.toStringAsFixed(2)}'),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Tip per person',
+                                    style: TextStyle(color: Colors.white)),
+                                Text(
+                                  _tipPerPerson.toStringAsFixed(2),
+                                  style: TextStyle(
+                                      color: Colors.blue.shade50,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                           Padding(
                             padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: Text('Total per person: ${_totPerPerson.toStringAsFixed(2)}'),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Total per person',
+                                    style: TextStyle(color: Colors.white)),
+                                Text(
+                                  _totPerPerson.toStringAsFixed(2),
+                                  style: TextStyle(
+                                      color: Colors.blue.shade50,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           )
                         ],
                       ),
@@ -141,17 +215,18 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(onPressed: (){
-                  setState(() {
-                    _billController.clear();
-                    _numPeopleController.clear();
-                    _totTip = 0.00;
-                    _tot = 0.00;
-                    _tipPerPerson = 0.00;
-                    _totPerPerson = 0.00;
-                  });
-
-                }, child: Text('Reset')),
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _billController.clear();
+                        _numPeopleController.clear();
+                        _totTip = 0.00;
+                        _tot = 0.00;
+                        _tipPerPerson = 0.00;
+                        _totPerPerson = 0.00;
+                      });
+                    },
+                    child: const Text('Reset')),
               ),
             ],
           ),
